c71f2797d5f49ee755dc7abe93125536f346b41f
sharsheev82 <sharsheev_ki@mail.ru>
2023-07-09 17:36:51 +0600

    add todos

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,6 @@ const ul = document.querySelector("ul")
 const addInput = document.querySelector(".add_input")
 const addButton = document.querySelector("add_button")
 
+const state = {
+    todos: []
+}
