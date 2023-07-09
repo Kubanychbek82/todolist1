0b5b47345ddae72f2c5f8dbef84e87b4672030d5
sharsheev82 <sharsheev_ki@mail.ru>
2023-07-09 18:39:46 +0600

    add function gettodos

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,3 +6,17 @@ const addButton = document.querySelector("add_button")
 const state = {
     todos: []
 }
+
+
+getTodods ()
+
+
+function getTodods () {
+    fetch("http://localhost:8000/todos")
+    .then((response) => response.json())
+    .then((data) => {
+        state.todos = data
+        render(state.todos)
+    })
+}
+
